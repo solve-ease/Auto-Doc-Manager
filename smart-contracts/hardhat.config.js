083770f78e42e041e1a50fe3db98d8d2da1af015
Adarsh <adarshmaurya9415@gmail.com>
2024-09-30 11:20:36 +0530

    alchemy test net setup

--- smart-contracts/hardhat.config.js
+++ smart-contracts/hardhat.config.js
@@ -3,20 +3,29 @@ require("@nomicfoundation/hardhat-toolbox")
 /** @type import('hardhat/config').HardhatUserConfig */
 require("dotenv").config()
 // ERRoR
-const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL
-const PRIVATE_KEY = process.env.PRIVATE_KEY
+// const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL
+// const PRIVATE_KEY = process.env.PRIVATE_KEY
 
-// console.log(process.env.PRIVATE_KEY)
-// console.log(typeof process.env.PRIVATE_KEY)
+const { API_URL, PRIVATE_KEY } = process.env
+
+console.log(process.env.PRIVATE_KEY)
+console.log(typeof process.env.PRIVATE_KEY)
 
 module.exports = {
     defaultNetwork: "hardhat",
+    // networks: {
+    //     sepolia: {
+    //         url: SEPOLIA_RPC_URL,
+    //         accounts: [PRIVATE_KEY],
+    //         chainId: 11155111,
+    //     },
+    // },
+    solidity: "0.8.8",
     networks: {
+        hardhat: {},
         sepolia: {
-            url: SEPOLIA_RPC_URL,
-            accounts: [PRIVATE_KEY],
-            chainId: 11155111,
+            url: API_URL,
+            accounts: [`0x${PRIVATE_KEY}`],
         },
     },
-    solidity: "0.8.8",
 }

a766a9d34033bc9aea5bdd39222631ad2df39d9e
AmanSingh494 <amanforwork1@gmail.com>
2025-01-14 18:02:44 +0530

    Fixed api url issue

--- frontend/src/pages/Docs.jsx
+++ frontend/src/pages/Docs.jsx
@@ -4,6 +4,8 @@ import contractInfo from '../../config/contractInfo.json'
 import DocCard from '../components/DocCard'
 import pako from 'pako'
 import { HashLoader } from 'react-spinners'
+const API_BASE_URL =
+  import.meta.env.VITE_API_BASE_URL || 'http://localhost:5000'
 
 const Docs = () => {
   const [signer, setSigner] = useState(null)
@@ -42,7 +44,7 @@ const Docs = () => {
     }
   }
   const getDocFromBackend = async (data) => {
-    const response = await fetch('http://localhost:5000/protected/get-docs', {
+    const response = await fetch(`${API_BASE_URL}/protected/get-docs`, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
